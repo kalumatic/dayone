845765b9f0cee27a322b9f9d6685b422df752b8d
kalu <maticluka888@gmail.com>
2024-04-30 16:28:13 +0200

    added current location and permission granting

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
 import "package:google_maps_flutter/google_maps_flutter.dart";
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -9,7 +11,17 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  Location _locationController = new Location();
+
   static const LatLng _MATF = LatLng(44.820048144999575, 20.458771869031093);
+
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +31,44 @@ class _MapPageState extends State<MapPage> {
             target: _MATF,
             zoom: 18,
           ),
-          markers:  {
+          markers: {
             Marker(
                 markerId: MarkerId("_currentLocation"),
                 icon: BitmapDescriptor.defaultMarker,
                 position: _MATF)
           }),
     );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          print(_currentP);
+        });
+      }
+    });
   }
 }
